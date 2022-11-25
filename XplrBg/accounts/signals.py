1e59e3d64f9da4b756931db882fa67fc2bf9f9f7
Marina Nenova <marinanenova89@gmail.com>
2022-11-25 18:27:36 +0200

    added a signal to delete profile photo on update

--- XplrBg/accounts/signals.py
+++ XplrBg/accounts/signals.py
@@ -1,6 +1,6 @@
+import cloudinary.uploader
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import UserProfile
 
@@ -16,3 +16,13 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=UserModel)
 def save_user_profile(sender, instance, **kwargs):
     instance.user_profile.save()
+
+
+@receiver(pre_save, sender=UserProfile)
+def deleting_old_profile_pic_on_update(sender, instance, **kwargs):
+    if sender.objects.filter(pk=instance.pk).exists():
+        if instance.pk:
+            old_profile_image = UserProfile.objects.get(pk=instance.pk).profile_image
+            new_profile_image = instance.profile_image
+            if old_profile_image and old_profile_image.url != new_profile_image.url:
+                cloudinary.uploader.destroy(old_profile_image.public_id)
