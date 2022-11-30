47bfff9e398fee4ac2d13640e5f46283ed8fc571
Marina Nenova <marinanenova89@gmail.com>
2022-11-30 15:27:10 +0200

    Created post comment model

--- XplrBg/posts_common/models.py
+++ XplrBg/posts_common/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from XplrBg.core.mixins.model_mixins import AuditInfoMixin
 from XplrBg.posts.models import Post
 
 UserModel = get_user_model()
+
 
 class PostLike(models.Model):
     post = models.ForeignKey(
@@ -15,5 +17,27 @@ class PostLike(models.Model):
 
     user = models.ForeignKey(
         UserModel,
-        on_delete=models.RESTRICT,
+        on_delete=models.CASCADE,
+    )
+
+
+class PostComment(AuditInfoMixin, models.Model):
+    MAX_TEXT_LENGTH = 300
+
+    text = models.CharField(
+        max_length=MAX_TEXT_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=True,
+    )
+
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
     )
